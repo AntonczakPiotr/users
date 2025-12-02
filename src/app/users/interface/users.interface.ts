9afe0fa2f3dcb09eb14cdaec99a6342209778a67
Piotr Antończak <AntonczakPiotr@gmail.com>
2025-12-03 00:19:02 +0100

    Dodanie enum, interface i tabeli z urzytkownikami

---
+++ src/app/users/interface/users.interface.ts
@@ -0,0 +1,8 @@
+import  { RoleEnum } from "../enums/role.enum"
+
+export interface UserInterface {
+    name: string,
+    surname: string,
+    role: string,
+    email: string
+}
--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RoleEnum } from './enums/role.enum';
+import { UserInterface } from './interface/users.interface';
 
 @Component({
   selector: 'app-users',
@@ -7,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './users.less',
 })
 export class Users {
-
+  users: UserInterface[] = [
+    {
+      name: 'Jan',
+      surname: 'Kowalski',
+      role: RoleEnum.admin,
+      email: 'test1@test.pl',
+    },
+    {
+      name: 'Karol',
+      surname: 'Kowalski',
+      role: RoleEnum.user,
+      email: 'test2@test.pl',
+    },
+    {
+      name: 'Monika',
+      surname: 'Nowak',
+      role: RoleEnum.user,
+      email: 'test3@test.pl',
+    },
+  ];
 }
